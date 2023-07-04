130dcf80fa2988285e382a6b240e0202de74da08
SinyavkiyV <vsinyavskiy94@gmail.com>
2023-07-04 14:28:57 +0700

    Add files via upload

---
+++ UtgardeKeepTrashRunic.lua
@@ -0,0 +1,20 @@
+local mod	= DBM:NewMod("UtgardeKeepTrash", "DBM-Party-WotLK", 10)
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision: 3933 $"):sub(12, -3))
+
+mod:RegisterEvents(
+	"SPELL_DAMAGE"
+)
+
+local specWarnBitingCold	= mod:NewSpecialWarningMove(62188, false)
+
+do 
+	local lastbitingcold = 0
+	function mod:SPELL_DAMAGE(args)
+		if args:IsSpellID(62038, 62188) and args:IsPlayer() and time() - lastbitingcold > 4 then		-- Biting Cold
+			specWarnBitingCold:Show()
+			lastbitingcold = time()
+		end
+	end
+end
